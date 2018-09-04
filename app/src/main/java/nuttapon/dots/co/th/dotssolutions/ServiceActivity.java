d3999ab814f6771dce030b9f3b7ba77bc48a5e33
masterUNG <phrombutr@gmail.com>
2018-09-04 11:38:13 +0700

    ForboonV1

--- app/src/main/java/nuttapon/dots/co/th/dotssolutions/ServiceActivity.java
+++ app/src/main/java/nuttapon/dots/co/th/dotssolutions/ServiceActivity.java
@@ -3,10 +3,18 @@ package nuttapon.dots.co.th.dotssolutions;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ServiceActivity extends AppCompatActivity {
 
-    private String jsonUserString;
+    private String jsonUserString, nameAnSurString, phoneString;
+    private MyConstant myConstant = new MyConstant();
+    private String[] columnStrings = myConstant.getColumnTcust();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +24,51 @@ public class ServiceActivity extends AppCompatActivity {
 //        Get Value From Preference
         getValueFromPreference();
 
+//        Create Toolbar
+        createToolbar();
+
+
     }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+
+        String firstName = findValueFromJSON(columnStrings[1]);
+        String lastName = findValueFromJSON(columnStrings[2]);
+        nameAnSurString = firstName + " " + lastName;
+
+        getSupportActionBar().setSubtitle(nameAnSurString);
+
+    }
+
+    private String findValueFromJSON(String columnString) {
+
+        String result = null;
+
+        try {
+
+            JSONArray jsonArray = new JSONArray(jsonUserString);
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            result = jsonObject.getString(columnString);
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Non";
+        }
+
+
+
+    }
 
     private void getValueFromPreference() {
 
         SharedPreferences sharedPreferences = getSharedPreferences("MyData", MODE_PRIVATE);
         jsonUserString = sharedPreferences.getString("User", "");
+        Log.d("4SepV1", "jsonUserString ==> " + jsonUserString);
+
+
 
 
     }
