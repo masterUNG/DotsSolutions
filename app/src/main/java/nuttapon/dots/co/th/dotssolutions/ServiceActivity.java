845b3112d19dc012d298245122fd61d47061ff32
masterUNG <phrombutr@gmail.com>
2018-09-04 11:01:38 +0700

    Forboon

--- app/src/main/java/nuttapon/dots/co/th/dotssolutions/ServiceActivity.java
+++ app/src/main/java/nuttapon/dots/co/th/dotssolutions/ServiceActivity.java
@@ -1,13 +1,30 @@
 package nuttapon.dots.co.th.dotssolutions;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ServiceActivity extends AppCompatActivity {
 
+    private String jsonUserString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Get Value From Preference
+        getValueFromPreference();
+
+    }   // Main Method
+
+    private void getValueFromPreference() {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyData", MODE_PRIVATE);
+        jsonUserString = sharedPreferences.getString("User", "");
+
+
     }
-}
+
+
+}   // Main Class
